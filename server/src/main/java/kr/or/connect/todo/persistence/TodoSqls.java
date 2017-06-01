1bd3327f244ee820888a07fab836198761e33c48
tn841 <tn841@naver.com>
2017-06-01 14:52:23 +0900

    오름차순 정렬을 위한 SQL문 수정

--- server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
+++ server/src/main/java/kr/or/connect/todo/persistence/TodoSqls.java
@@ -1,10 +1,9 @@
 package kr.or.connect.todo.persistence;
 
 public class TodoSqls {
-	static final String SELECT_ALL = "SELECT * FROM todo";
-	static final String SELECT_BY_COMPLETED = "SELECT * FROM todo WHERE completed=:completed";
+	static final String SELECT_ALL = "SELECT * FROM todo ORDER BY date";	//오름차순 정렬
+	static final String SELECT_BY_COMPLETED = "SELECT * FROM todo WHERE completed=:completed ORDER BY date";
 	static final String DELETE_BY_ID ="DELETE FROM todo WHERE id= :id";
 	static final String UPDATE_BY_ID = "UPDATE todo SET todo=:todo, completed=:completed WHERE id=:id";
 	static final String DELETE_BY_COMPLETED = "DELETE FROM todo WHERE completed=:completed";
-	static final String SELECT_BY_FILTER = "SELECT * FROM todo WHERE completed=:completed";
 }
